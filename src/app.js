84312067d5b569d446174480708a14e234dbb455
Anyhara <163209010+Anyhara@users.noreply.github.com>
2024-05-04 15:53:30 +0000

    lineamiento de codigo

--- src/app.js
+++ src/app.js
@@ -7,20 +7,9 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
   document.querySelector("#excuse").innerHTML = excusegen();
 };
 
-let who = ["The dog", "My grandma", "The mailman", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "my phone", "the car"];
-let when = [
-  "before the class",
-  "when I was sleeping",
-  "while I was exercising",
-  "during my lunch",
-  "while I was praying"
-];
 function excusegen() {
   let who = ["The dog", "My grandma", "The mailman", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
@@ -32,9 +21,9 @@ function excusegen() {
     "during my lunch",
     "while I was praying"
   ];
-  let who1 = Math.floor(Math.random() * who.length);
-  let action1 = Math.floor(Math.random() * action.length);
-  let what1 = Math.floor(Math.random() * what.length);
-  let when1 = Math.floor(Math.random() * when.length);
-  return ` ${who[who1]} ${action[action1]} ${what[what1]} ${when[when1]}`;
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+  return ` ${who[randomWho]} ${action[randomAction]} ${what[randomWhat]} ${when[randomWhen]}`;
 }
